5d7d7953378f68fdad2b138a69c55b1b40487965
ZimuW <zimu@quip.com>
2018-10-28 23:04:20 -0700

    Add more code

--- warmup/Conway/main.go
+++ warmup/Conway/main.go
@@ -1,20 +1,42 @@
 // An implementation of Conway's Game of Life in Commandline
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Field struct {
-	s    [][]bool
-	w, h int
+	board         [][]bool
+	width, height int
 }
 
-func newField(w, h int) *Field {
-	s := make([][]bool, h)
-	for i := range s {
-		s[i] = make([]bool, w)
+func newField(width, height int) *Field {
+	board := make([][]bool, height)
+	for i := range board {
+		board[i] = make([]bool, width)
 	}
-	return &Field{s: s, w: w, h: h}
+	return &Field{board: board, width: width, height: height}
 }
+
+func (f *Field) Set(x, y int, b bool) {
+	f.board[x][y] = b
+}
+
+type Life struct {
+	old, new      *Field
+	width, height int
+}
+
+func randomNewLife(width, height int) *Life {
+	old := newField(width, height)
+	for i := 0; i < (width * height / 4); i++ {
+		old.Set(rand.Intn(width), rand.Intn(height), true)
+	}
+	return &Life{old: old, new: newField(width, height), width: width, height: height}
+}
+
+// func newLifeFromConfig(width, height int) *Life {}
 
 func main() {
 	fmt.Print("HERE")
